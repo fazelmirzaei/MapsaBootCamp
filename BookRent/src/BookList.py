274f8f763a390e3ecbe535877be8dd961c60f780
fazel <fazelmirzayi@gmail.com>
2019-12-05 16:06:21 +0330

    Changing Scratch files to mine

--- BookRent/src/BookList.py
+++ BookRent/src/BookList.py
@@ -1,20 +1,76 @@
 class Book:
+    book_count=0
 
-    def __init__(self, name, author, international,   category, bookId):
+    def __init__(self, name,author,category,international):
         self.name = name
         self.author = author
         self.category = category
-        self.bookId = bookId
-        self.status = True
+        self.status = None # thats explain how book is rented or not
+        self.international=international
+        self.bookID = None
         self.rent = None
-        self.international = international
-        self.motarjem = None
+        Book.book_count+=1
 
-    def rentBook(self, member):
-        pass
+    def setCategori(self,category):
+        if self.category==None:
+            self.category=category
+            print("The category is assigned to {}".format(category))
+        else:
+            print("The Category had been assigned earlier and it is {}".format(self.category))
 
-    def setMotarjem(self, motarjem):
-        if self.international:
-            self.motarjem = motarjem
+    def rentbook(self,member):  # its task is in charge of validating membrance and having book or not
+        if self.status==None:
+            self.status= member
+            print("this book is available and  now rented to {}".format(member))
+        else:
+            print("This book was already rented to {}".format(self.status))
 
-# Todo method?   attribute?     class??
+
+    #------------------------------------------------------------------------------------------------------
+class BookIrani(Book):
+    persianBook_count=0    #(can)t use!!
+    def __init__(self,name, author, category, international=False):
+        Book.__init__(self,name,author,category,international)
+        self.pub_year=None
+        self.edition=None
+        self.serial=None
+        self.translator_name=None
+        self.publisher=None
+        self.price=None
+        self.serial=None
+        BookIrani.persianBook_count+=1
+
+    def set_Pub_year(self,pub_yaer):
+        if self.pub_year==None:
+            self.pub_year=pub_yaer
+        return  pub_yaer
+    def set_Publisher(self,publisher):
+        if self.publisher==None:
+            self.publisher=publisher
+        return publisher
+    def set_Price(self,price):
+        if self.price==None:
+            self.price=price
+        return price
+    def set_Serial(self,serial):
+        if self.serial==None:
+            self.serial=serial
+    # def display_Pcount(self):
+    #     print("we have {} books in our library".format(Book.persianBook_count))
+
+class BookInternational(Book):
+    def __init__(self,name,author,category,international=True):
+        Book.__init__(self, name, author, category, international)
+        self.translator_name=None
+        self.edition=None
+    def setTranslator(self,translator_name):
+        if  self.international==True:
+            self.translator_name = translator_name
+            print('the translator has been assigned to {}'.format(translator_name))
+        else:
+            print("This book is not international and there is no need to translator.")
+
+
+b1=BookIrani('Divane Hafez','Hafez Shirazi',None,0)
+b1.setCategori('Poetry')
+b1.setCategori('Science')
--- BookRent/src/Members.py
+++ BookRent/src/Members.py
@@ -1,16 +1,60 @@
 import datetime
+import random
+class Members(object):
 
-
-class Members:
     def __init__(self, name, age):
-        self. name = name
+        self.name = name
         self.age = age
-        self.iD = None
+        self.date = datetime.datetime.now().date()
+        self.iD = None  ## because its value is None, there is no need to define in def section as argument
         self.rent = None
-        self.date = datetime.datetime.now()
+
+   # def idGenerator(self):
+   #     if self.iD is None:
+   #         self.iD = self.date.year + self.date.month + self.date.day + self.date.minute
+   #         print('your ID is {}'.format(self.iD))
+   #     else:
+   #         print('This member hasbeen found in our database')
+   #
+   #      self.iD += 1
 
     def idGenerator(self):
-        pass
+        if self.iD==None:
+            self.iD= ''.join(random.sample('0123456789', 5))
+            print('Id is: '+ self.iD)
+        else:
+            for i,j in Members():
+                if self.iD[i]==self.iD[j]:
+                    print('its a douplicated Id, please try again ')
+                else:
+                    return
+            return
+
+
+#### Ques: How can I make my class's object iterable???
 
     def expireCheck(self):
-        pass
+        regdt=datetime.datetime.now().date()
+        duedt=datetime.datetime.today().date()
+        a=10000*duedt.year+100*duedt.month+duedt.day
+        b=10000*regdt.year+100*regdt.month+regdt.day
+
+        if a-b>= 10000:
+            print('Please register as soon as possible')
+        else:
+            print('your registeration is validate')
+
+
+
+p1 = Members('hassan', 25)
+p2=Members('ali',88)
+p3=Members('fazel',77)
+
+p3.idGenerator()
+p1.expireCheck()
+
+
+
+
+
+
